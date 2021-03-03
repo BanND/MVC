debd545d6eb95d7ab5a226d1ed1e9c12cc89efd7
Ban Nguyen <ban.nguyen@episerver.com>
2021-03-03 14:42:54 +0700

    Access Data Test

--- Controllers/StandardPageCopyController.cs
+++ Controllers/StandardPageCopyController.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Web.Mvc;
 using EPiServer.Core;
-using MVC.Models.Pages;
-using MVC.Models.ViewModels;
+using EPiServer.SpecializedProperties;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
+using MVC.Models.Pages;
+using MVC.Models.ViewModels;
+using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
-    public class StartPageController : PageControllerBase<StartPage>
+    public class StandardPageCopyController : PageControllerBase<StandardPageCopy>
     {
-        public ActionResult Index(StartPage currentPage)
+        public ActionResult Index(StandardPageCopy currentPage)
         {
             var model = PageViewModel.Create(currentPage);
 
@@ -18,7 +18,10 @@ namespace MVC.Controllers
             PropertyData property = currentPage.Property["TestProp1Description"];
             if (property != null && !property.IsNull)
             {
-                var editHints = ViewData.GetEditHints<PageViewModel<StartPage>, StartPage>();
+                var editHints = ViewData.GetEditHints<PageViewModel<StandardPageCopy>, StandardPageCopy>();
+                editHints.AddConnection(m => m.CurrentPage.TestProp1, p => p.TestProp1);
+                editHints.AddConnection<LinkItemCollection>(m => m.Layout.CustomerZonePages, p => new LinkItemCollection());
+
             }
 
             var propertyValueAsString = currentPage["TestProp1Description"] as string;
@@ -30,12 +33,9 @@ namespace MVC.Controllers
             if (SiteDefinition.Current.StartPage.CompareToIgnoreWorkID(currentPage.ContentLink)) // Check if it is the StartPage or just a page of the StartPage type.
             {
                 //Connect the view models logotype property to the start page's to make it editable
-                var editHints = ViewData.GetEditHints<PageViewModel<StartPage>, StartPage>();
-                editHints.AddConnection(m => m.Layout.Logotype, p => p.SiteLogotype);
-                editHints.AddConnection(m => m.Layout.ProductPages, p => p.ProductPageLinks);
-                editHints.AddConnection(m => m.Layout.CompanyInformationPages, p => p.CompanyInformationPageLinks);
-                editHints.AddConnection(m => m.Layout.NewsPages, p => p.NewsPageLinks);
-                editHints.AddConnection(m => m.Layout.CustomerZonePages, p => p.CustomerZonePageLinks);
+                var editHints = ViewData.GetEditHints<PageViewModel<StandardPageCopy>, StandardPageCopy>();
+                editHints.AddConnection(m => m.CurrentPage.TestProp1, p => p.TestProp1);
+                editHints.AddConnection<LinkItemCollection>(m => m.Layout.CustomerZonePages, p => new LinkItemCollection());
             }
 
             return View(model);
--- Models/Pages/StandardPageCopy.cs
+++ Models/Pages/StandardPageCopy.cs
@@ -1,3 +1,4 @@
+using System;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
@@ -23,9 +24,9 @@ namespace MVC.Models.Pages
             Order = 320)]
         public virtual ContentArea MainContentArea { get; set; }
 
-        public string TestProp1 { get; set; }
+        public virtual string TestProp1 { get; set; }
 
-        public int TestProp2 { get; set; }
+        public virtual int TestProp2 { get; set; }
 
         [CultureSpecific]
         public virtual string TestProp1Description
@@ -37,6 +38,61 @@ namespace MVC.Models.Pages
                 return string.IsNullOrWhiteSpace(propertyValue) ? (string)this["TestProp2"] : propertyValue;
             }
             set => this["TestProp1"] = value;
+            //set { this.SetPropertyValue(p => p.TestProp1, value); }
+        }
+
+        [CultureSpecific]
+        public virtual string TestProp2Description
+        {
+            get
+            {
+                var propertyValue = this["TestProp2"] as string;
+
+                return string.IsNullOrWhiteSpace(propertyValue) ? (string)this["TestProp2"] : propertyValue;
+            }
+            set => this["TestProp1"] = value;
+        }
+
+        public virtual string GetPropertyValueTest
+        {
+            get
+            {
+                // 1. Get as string, defaults to null and otherwise calls ToString for the value
+                string mainBody = this.GetPropertyValue("MainBody");
+
+                // 2. Specify a fallback value
+                string mainBodyFallback = this.GetPropertyValue("MainBody", string.Empty);
+
+                // 3. Which is equivalent to
+                string mainBodyFallback2 = this.GetPropertyValue("MainBody") ?? string.Empty;
+
+                // 4. So a common usage is probably
+                string pageHeading = this.GetPropertyValue("PageHeading", this.PageName);
+
+                // 5. Get typed value, defaults to null if property is not set or defined
+                XhtmlString xhtml = this.GetPropertyValue<XhtmlString>("MainBody");
+
+                // 6. Which supports fallback in the same way as the "ToString" overload
+                XhtmlString xhtmlWithFallback =
+                    this.GetPropertyValue<XhtmlString>("MainBody", new XhtmlString());
+
+                // 7. Advanced: Specify a conversion for the value
+                // Note: this example is very similar to 1. since we just call ToString, EXCEPT
+                // if MainBody is something else than an Xhtml property in which case 1. would still
+                // return the ToString representation while this would throw an exception since
+                // it tries to cast the value to an XhtmlString before handing it over to the
+                // conversion lambda
+                string convertedXhtml =
+                    this.GetPropertyValue<XhtmlString, string>("MainBody", x => x.ToString());
+
+                // 8. This is equivalent to 1. since it treats the property value as object
+                string mainBody2 =
+                    this.GetPropertyValue<object, string>("MainBody", x => x.ToString());
+
+                return string.Empty;
+            }
+
+            set { this.SetPropertyValue(p => p.GetPropertyValueTest, value); }
         }
     }
 }
